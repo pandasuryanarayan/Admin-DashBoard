f5fc73c3c24742c4eec3da7ebd004a3ed35824cd
Suryanarayan Panda <116947409+pandasuryanarayan@users.noreply.github.com>
2025-03-23 00:19:44 +0530

    Update api.js

--- services/api.js
+++ services/api.js
@@ -5,8 +5,9 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 // To run in local machine, proxy is needed to restrict CORS.
 // You can implement your Proxy Server or use "https://thingproxy.freeboard.io/fetch/" free proxy server for testing and development.
 // Uncomment below code to use in development and testing.
-const apiUrl = `https://thingproxy.freeboard.io/fetch/${process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_API}`;
+// const apiUrl = `https://thingproxy.freeboard.io/fetch/${process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_API}`;
 // const apiUrl = `${process.env.NEXT_PUBLIC_ADMIN_DASHBOARD_API}`;
+const apiUrl = '/proxy';
 
 // Fetch all data from the dashboard API
 export function useDashboardData() {
